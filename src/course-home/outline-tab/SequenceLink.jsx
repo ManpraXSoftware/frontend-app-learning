c1620466461c6183dce135a7267e531069ee76c2
Om <om.kumar@manprax.com>
2026-06-17 16:48:44 +0530

    added css for view progress btn

--- src/course-home/outline-tab/SequenceLink.jsx
+++ src/course-home/outline-tab/SequenceLink.jsx
@@ -151,7 +151,7 @@ const SequenceLink = ({
 
         {!showAssmt && useProgramThreshold?
         (<div className="row w-100 m-0 ml-3 pl-3">
-          <small className="text-body pl-2" tabindex="0" aria-label="Program Assessment is locked. To unlock it, you need to complete all courses in the program first.">
+          <small className="text-body pl-2 prog-lock-msg" tabindex="0" aria-label="Program Assessment is locked. To unlock it, you need to complete all courses in the program first.">
             Program Assessment is locked. To unlock it, you need to complete all courses in the program first.
             {' '}
             <a
